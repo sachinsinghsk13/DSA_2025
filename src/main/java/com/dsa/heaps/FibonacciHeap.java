8e4cc8dffb19a8ec96afcfd924bea22e1428d10b
Sachin Singh <sachinsingh.sk13@gmail.com>
2025-04-14 23:48:36 +0530

    Fibonacci Heap done

--- src/main/java/com/dsa/heaps/FibonacciHeap.java
+++ src/main/java/com/dsa/heaps/FibonacciHeap.java
@@ -1,18 +1,20 @@
 package com.dsa.heaps;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class FibonacciHeap {
   private FibonacciNode min;
   private int n = 0;
+  private Map<Integer, FibonacciNode> nodeLocator = new HashMap<>();
 
   // O(1)
   public void insert(int key) {
     FibonacciNode node = new FibonacciNode(key);
-    if (min == null)  {
+    if (min == null) {
       min = node;
     } else {
       node.left = min;
@@ -24,11 +26,15 @@ public class FibonacciHeap {
       min = node;
     }
     n++;
+    // Add to map
+    this.nodeLocator.put(key, node);
   }
 
   // O(1)
   public int findMin() {
-    if (min == null) throw  new NoSuchElementException("Key not found");
+    if (min == null) {
+      throw new NoSuchElementException("Key not found");
+    }
     return min.key;
   }
 
@@ -67,24 +73,11 @@ public class FibonacciHeap {
 
   public int extractMin() {
     // Empty heap
-    if (min == null) throw new NoSuchElementException("Empty Heap");
-
-    // Single key heap
-    if (min.left == min) {
-      int value = min.key;
-      min = null;
-      n = 0;
-      return value;
+    if (min == null) {
+      throw new NoSuchElementException("Empty Heap");
     }
 
     FibonacciNode temp = min;
-    // Remove min node from main root list.
-    min.left.right = min.right;
-    min.right.left = min.left;
-
-    // let min point the adjacent node.
-    min = min.left;
-
     // Move children to the main root list.
     if (temp.child != null) {
       FibonacciNode ptr = temp.child;
@@ -105,11 +98,22 @@ public class FibonacciHeap {
         ptr = next;
       } while (ptr != temp.child);
     }
-    n = n - 1;
-    if (n > 1) {
+
+    // Remove min node from main root list.
+    min.left.right = min.right;
+    min.right.left = min.left;
+
+    if (min.left == min) {
+      min = null;
+    } else {
+      // let min point the adjacent node.
+      min = min.left;
       consolidateHeap();
     }
+
+    n = n - 1;
     temp.parent = temp.child = temp.left = temp.right = null;
+    nodeLocator.remove(temp.key); // remove from map
     return temp.key;
   }
 
@@ -144,23 +148,23 @@ public class FibonacciHeap {
     }
     min = null;
     for (FibonacciNode fibonacciNode : degreeNodeList) {
-        if (fibonacciNode != null) {
-            if (min == null) {
-              min = fibonacciNode;
-              fibonacciNode.left = fibonacciNode;
-              fibonacciNode.right = fibonacciNode;
-            } else {
-              // place new node after the min node
-              min.right.left = fibonacciNode;
-              fibonacciNode.right = min.right;
-              fibonacciNode.left = min;
-              min.right = fibonacciNode;
+      if (fibonacciNode != null) {
+        if (min == null) {
+          min = fibonacciNode;
+          fibonacciNode.left = fibonacciNode;
+          fibonacciNode.right = fibonacciNode;
+        } else {
+          // place new node after the min node
+          min.right.left = fibonacciNode;
+          fibonacciNode.right = min.right;
+          fibonacciNode.left = min;
+          min.right = fibonacciNode;
 
-              if (fibonacciNode.key < min.key) {
-                min = fibonacciNode;
-              }
-            }
+          if (fibonacciNode.key < min.key) {
+            min = fibonacciNode;
+          }
         }
+      }
     }
 
   }
@@ -185,5 +189,66 @@ public class FibonacciHeap {
     y.parent = x;
     x.degree = x.degree + 1;
   }
+
+  public void decreaseKey(int key, int newValue) {
+    if (newValue > key) {
+      throw new RuntimeException("Key is greater than current key");
+    }
+    if (!nodeLocator.containsKey(key)) {
+      throw new RuntimeException("Key not found");
+    }
+    FibonacciNode ptr = nodeLocator.get(key);
+    ptr.key = newValue;
+    FibonacciNode parent = ptr.parent;
+    if (parent != null && ptr.key < parent.key) {
+      cut(ptr, parent);
+      cascadingCut(parent);
+    } if (min == null || newValue < min.key) {
+      min = ptr;
+    }
+    // update map
+    nodeLocator.remove(key);
+    nodeLocator.put(newValue, ptr);
+  }
+
+  private void cascadingCut(FibonacciNode parent) {
+    FibonacciNode g = parent.parent;
+    if (g != null) {
+      if (!parent.mark) {
+        parent.mark = true;
+      } else {
+        cut(parent, g);
+        cascadingCut(g);
+      }
+    }
+  }
+
+  private void cut(FibonacciNode ptr, FibonacciNode parent) {
+    // if it's single child
+    if (ptr.left == ptr) {
+      parent.child = null;
+    } else {
+      // remove this node from child root list.
+      ptr.left.right = ptr.right;
+      ptr.right.left = ptr.left;
+      if (parent.child == ptr) { // let parent point to a sibling child.
+        parent.child = ptr.left;
+      }
+    }
+
+    parent.degree = parent.degree - 1;
+    ptr.parent = null;
+
+    // insert into root list.
+    min.right.left = ptr;
+    ptr.right = min.right;
+    min.right = ptr;
+    ptr.left = min;
+
+    // mark node as false.
+    ptr.mark = false;
+  }
+
+
 
 }
