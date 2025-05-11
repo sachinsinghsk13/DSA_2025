b979766a76ecd27cdd7fa538c57923389f349c60
Sachin Singh <sachinsingh.sk13@gmail.com>
2025-05-11 19:36:18 +0530

    kruskals algorithm implemented

--- src/main/java/com/dsa/Driver.java
+++ src/main/java/com/dsa/Driver.java
@@ -1,5 +1,6 @@
 package com.dsa;
 
+import com.dsa.graphs.AdjacencyListGraph;
 import com.dsa.graphs.AdjacencyMatrixGraph;
 import com.dsa.graphs.DisjointSet;
 import com.dsa.graphs.WeightedGraph;
@@ -9,11 +10,17 @@ import java.util.Arrays;
 public class Driver {
 
   public static void main(String[] args) {
-    DisjointSet set = new DisjointSet(8);
-    set.union(0, 3);
-    set.union(4, 5);
-    set.union(5, 0);
-    System.out.println(set.belongsToSameSet(0, 4));
+    WeightedGraph g = new AdjacencyListGraph(6, false);
+    g.addEdge(0, 1, 7);
+    g.addEdge(0, 2, 6);
+    g.addEdge(0, 3, 1);
+    g.addEdge(1, 2, 8);
+    g.addEdge(2, 3, 5);
+    g.addEdge(2, 4, 3);
+    g.addEdge(3, 5, 5);
+    g.addEdge(4, 5, 2);
+    g.addEdge(4, 3, 4);
+    MinimumSpanningTree.kruskalsMST(g).forEach(System.out::println);
   }
 }
 
--- src/main/java/com/dsa/graphs/algorithms/MinimumSpanningTree.java
+++ src/main/java/com/dsa/graphs/algorithms/MinimumSpanningTree.java
@@ -1,5 +1,6 @@
 package com.dsa.graphs.algorithms;
 
+import com.dsa.graphs.DisjointSet;
 import com.dsa.graphs.WeightedEdge;
 import com.dsa.graphs.WeightedGraph;
 import java.util.ArrayList;
@@ -16,19 +17,49 @@ public class MinimumSpanningTree {
     queue.add(new WeightedEdge(-1, 0, 0));
     while (!queue.isEmpty()) {
       WeightedEdge edge = queue.poll();
-      if (visited[edge.dest]) continue;
+      if (visited[edge.dest]) {
+        continue;
+      }
       visited[edge.dest] = true;
       if (edge.src >= 0) {
         mstEdges.add(edge);
       }
 
-      for (WeightedEdge e: g.getNeighboursWithWeight(edge.dest)) {
+      for (WeightedEdge e : g.getNeighboursWithWeight(edge.dest)) {
         if (!visited[e.dest]) {
           queue.add(e);
         }
       }
     }
     return mstEdges;
+  }
+
+  public static List<WeightedEdge> kruskalsMST(WeightedGraph g) {
+    // collect the edges
+    List<WeightedEdge> mst = new ArrayList<>();
+    PriorityQueue<WeightedEdge> pq = new PriorityQueue<>();
+    for (int vertex = 0; vertex < g.totalVertices(); vertex++) {
+      List<WeightedEdge> edges = g.getNeighboursWithWeight(vertex);
+      for (WeightedEdge edge : edges) {
+        if (edge.getSrc() < edge.getDest()) {
+          pq.add(edge);
+        }
+      }
+    }
+
+    DisjointSet ds = new DisjointSet(g.totalVertices());
+    while (!pq.isEmpty()) {
+      WeightedEdge edge = pq.poll();
+      if (ds.belongsToSameSet(edge.src, edge.dest)) {
+        continue;
+      }
+      mst.add(edge);
+      ds.union(edge.src, edge.dest);
+      if (mst.size() == g.totalVertices() - 1) {
+        break;
+      }
+    }
+    return mst;
   }
 
 }
